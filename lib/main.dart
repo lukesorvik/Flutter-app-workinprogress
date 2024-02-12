3a27e0f64f5e92c5a28643a725d264ef0d431185
Luke Sorvik <139653155+lukesorvik@users.noreply.github.com>
2024-02-11 17:33:47 -0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart'; //widgets that let you mess with app
 void main() {
   runApp(myApp());
 }
-
+//
 //stateless widget that doesnt do anything
 class myApp extends StatelessWidget {
   const myApp({super.key});
